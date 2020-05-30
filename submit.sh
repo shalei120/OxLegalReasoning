13de260101b60b6aa3e3643fdf82688d325f26f3
shalei120 <shalei120@sina.com>
2020-05-30 23:22:04 +0800

    add submit

---
+++ submit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --partition=small
+#SBATCH --job-name=Legal
+#SBATCH --ntasks-per-node=5
+#SBATCH --gres=gpu:1
+
+module load python/anaconda3/2019.03
+module load gpu/cuda/10.1.243
+module load gpu/cudnn/7.5.0__cuda-10.0
+echo "CUDA Device(s) : $CUDA_VISIBLE_DEVICES"
+nvidia-smi
+
+# run the application
+python3 main.py -m transformer
